e4e0117cde5362b036af55b21d02f051c9be7da0
alfario207 <alfariolilhuda@gmail.com>
2026-01-11 11:32:24 +0700

    update button per ruang

--- saklar.js
+++ saklar.js
@@ -1,4 +1,7 @@
-let isActive = false;
+let isActive = false
+let isActiveRuangKeluarga = false
+let isActiveRuangTidur =  false
+let isActiveRuangMakan = false
 
 function saklar() {
     let toggle1 = document.getElementById("default-toggle1")
@@ -22,7 +25,7 @@ function saklar() {
     let lampu8 = document.getElementById("lampu8")
     let lampu9 = document.getElementById("lampu9")
     let lampu10 = document.getElementById("lampu10")
- console.log('toggle1', toggle1.checked)
+
     if (toggle1.checked) {
         lampu1.src = "assets/images/on.gif"
     } else {
@@ -85,38 +88,30 @@ function saklar() {
 }
 
 
-
 function handleBtn() {
-    isActive=!isActive;
-    //1
-    // isActive=false
-    //2
-    // isActive=true
-    //3
-    // isActive=false
-    // console.log('Current:',isActive)
+    let toggle1 = document.getElementById("default-toggle1")
+    let toggle2 = document.getElementById("default-toggle2")
+    let toggle3 = document.getElementById("default-toggle3")
+    let toggle4 = document.getElementById("default-toggle4")
 
+    // jika salah satu toggle true, btn dianggap true
+    // jika btn diklik berubah jadi false
+    
+    if(toggle1.checked|toggle2.checked|toggle3.checked|toggle4.checked) {
+        isActive = false
+    } else {
+        isActive=!isActive;
+    }
 }
 
-function ruang() {
+function ruangTamu() {
     console.log('BUTTON CLICKED!')
     handleBtn()
-
     let ruangTamu = document.getElementById("button-ruangtamu")
-    let ruangKeluarga = document.getElementById ("button-ruangkeluarga")
-    let ruangTidur = document.getElementById("button-ruangmakan") 
-
     let lampu1 = document.getElementById("lampu1")
     let lampu2 = document.getElementById("lampu2")
     let lampu3 = document.getElementById("lampu3")
     let lampu4 = document.getElementById("lampu4")
-    let lampu5 = document.getElementById("lampu5")
-    let lampu6 = document.getElementById("lampu6")
-    let lampu7 = document.getElementById("lampu7")
-    let lampu8 = document.getElementById("lampu8")
-    let lampu9 = document.getElementById("lampu9")
-    let lampu10 = document.getElementById("lampu10")
-
     let toggle1 = document.getElementById("default-toggle1")
     let toggle2 = document.getElementById("default-toggle2")
     let toggle3 = document.getElementById("default-toggle3")
@@ -144,5 +139,121 @@ function ruang() {
         toggle2.checked = false
         toggle3.checked = false
         toggle4.checked = false
+    } 
+}
+
+
+function handleBtnKeluarga() {
+    let toggle5 = document.getElementById("default-toggle5")
+    let toggle6 = document.getElementById("default-toggle6")
+    let toggle7 = document.getElementById("default-toggle7")
+
+    console.log('IS ACTIVE HANDLE BTN KLG',isActiveRuangKeluarga)
+    if(toggle5.checked|toggle6.checked|toggle7.checked){
+        isActiveRuangKeluarga = false
+    } else {
+        isActiveRuangKeluarga=!isActiveRuangKeluarga
+    }
+
+}
+
+function ruangKeluarga() {
+    console.log('BUTTON CLICK')
+    handleBtnKeluarga()
+    let ruangKeluarga = document.getElementById("button-ruangkeluarga")
+    let lampu5 = document.getElementById("lampu5")
+    let lampu6 = document.getElementById("lampu6")
+    let lampu7 = document.getElementById("lampu7")
+    let toggle5 = document.getElementById("default-toggle5")
+    let toggle6 = document.getElementById("default-toggle6")
+    let toggle7 = document.getElementById("default-toggle7")
+
+    // console.log('ruangKeluarga', ruangKeluarga.checked)
+    console.log('ruangKeluarga IS ACTIVE2', isActiveRuangKeluarga)
+    console.log('toggle5', toggle5.checked)
+    console.log('toggle6', toggle6.checked)
+    console.log('toggle7', toggle7.checked)
+
+    if(isActiveRuangKeluarga) {
+        lampu5.src = "assets/images/on.gif"
+        lampu6.src = "assets/images/on.gif"
+        lampu7.src = "assets/images/on.gif"
+
+        toggle5.checked = true
+        toggle6.checked = true
+        toggle7.checked = true
+    } else {
+        lampu5.src = "assets/images/off.gif"
+        lampu6.src = "assets/images/off.gif"
+        lampu7.src = "assets/images/off.gif"
+
+        toggle5.checked = false
+        toggle6.checked = false
+        toggle7.checked = false
+    }
+}
+
+
+function handleBtnTidur () {
+    let toggle8 = document.getElementById("default-toggle8") 
+    let toggle9 = document.getElementById("default-toggle9")
+
+    if(toggle8.checked|toggle9.checked){
+        isActiveRuangTidur = false
+    } else {
+        isActiveRuangTidur=!isActiveRuangTidur
+    }
+}
+
+function ruangTidur() {
+    console.log('BUTTON DI CLICK')
+    handleBtnTidur()
+    let ruangTidur = document.getElementById("button-ruangtidur")
+    let lampu8 = document.getElementById("lampu8")
+    let lampu9 = document.getElementById("lampu9")
+    let toggle8 = document.getElementById("default-toggle8")
+    let toggle9 = document.getElementById("default-toggle9")
+
+    console.log('ruangTidur', ruangTidur.checked)
+
+    if(isActiveRuangTidur) {
+        lampu8.src = "assets/images/on.gif"
+        lampu9.src = "assets/images/on.gif"
+
+        toggle8.checked = true
+        toggle9.checked = true
+    } else {
+        lampu8.src = " assets/images/off.gif"
+        lampu9.src = " assets/images/off.gif"
+
+        toggle8.checked = false
+        toggle9.checked = false
+    }
+}
+
+function handleBtnMakan () {
+    let toggle10 = document.getElementById("default-toggle10")
+    if(toggle10.checked) {
+        isActiveRuangMakan = false
+    } else {
+        isActiveRuangMakan=!isActiveRuangMakan
+    }
+}
+
+function ruangMakan() {
+    console.log('BUTTON-NYA DI-CLICK')
+    handleBtnMakan()
+    let ruangMakan = document.getElementById("button-ruangmakan")
+    let lampu10 = document.getElementById("lampu10")
+    let toggle10 = document.getElementById("default-toggle10")
+
+    console.log('ruangMakan', ruangMakan.checked)
+
+    if(isActiveRuangMakan) {
+        lampu10.src = "assets/images/on.gif"
+        toggle10.checked = true
+    } else {
+        lampu10.src = "assets/images/off.gif"
+        toggle10.checked = false
     }
 }
